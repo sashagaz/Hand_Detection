2cc990e2a17ab5885853ff343548fd1eb17d8f25
Esteban Martinena Guerrero <orensbruli@gmail.com>
2019-07-23 11:40:24 +0200

    Added from_frame  static method and SIDE class to get roi from frames with a side and percentage. Added some test to main.

--- roi.py
+++ roi.py
@@ -1,5 +1,11 @@
 
 
+class SIDE:
+    RIGHT = 0
+    LEFT = 1
+    TOP = 2
+    BOTTOM = 3
+
 class Roi(object):
     def __init__(self):
         self._x = 0
@@ -15,6 +21,33 @@ class Roi(object):
     def params(self, params):
         self._x, self._y, self._width, self._height = (params[0], params[1], params[2], params[3])
 
+    @staticmethod
+    def from_frame(frame, side, percent):
+        new_roi = Roi()
+        assert isinstance(side, int) and 0 <= side < 4, "Side must be a value between 0 and 3 but %d given. Use the SIDE class to get valid values." % side
+        assert isinstance(percent, (int, float)) , "Percent must be must be Integer or Float but %r given" % percent
+        assert (0 < percent <= 100), "Percent must be a value between 0 and 100 but %d given" % percent
+        frame_width, frame_height = frame.shape[:2]
+        # calculating percentage of the frame width and height
+        frame_width_percent = frame_width * percent / 100
+        frame_height_percent = frame_height * percent / 100
+        if side == SIDE.TOP:
+            x, y = (0,0)
+            width, height = (frame_width, frame_height_percent)
+        elif side == SIDE.BOTTOM:
+            x, y = (0, frame_height-frame_height_percent)
+            width, height = (frame_width, frame_height_percent)
+        elif side == SIDE.RIGHT:
+            x, y = (frame_width-frame_width_percent, 0)
+            width, height = (frame_width_percent, frame_height)
+        elif side == SIDE.LEFT:
+            x, y = (0, 0)
+            width, height = (frame_width_percent, frame_height)
+        new_roi.params = (x, y, width, height)
+        return new_roi
+
+    def __str__(self):
+        return str(self.params)
 
     def upscaled(self, limiting_roi, upscaled_pixels):
         """
@@ -41,10 +74,10 @@ class Roi(object):
         else:
             exceded_pixels = y + h + upscaled_pixels - limiting_roi[0]
             new_h = h + exceded_pixels
-        upscaled_bounding_rect = Roi()
-        upscaled_bounding_rect.params = (new_x, new_y, new_w, new_h)
+        upscaled_roi = Roi()
+        upscaled_roi.params = (new_x, new_y, new_w, new_h)
 
-        return upscaled_bounding_rect
+        return upscaled_roi
 
     def downscaled(self, limiting_roi, downscaled_pixels):
         """
@@ -68,7 +101,38 @@ class Roi(object):
             new_h = h + downscaled_pixels
         else:
             new_h = 0
-        downscaled_bounding_rect = Roi()
-        downscaled_bounding_rect.params = (new_x, new_y, new_w, new_h)
-        return downscaled_bounding_rect
+        downscaled_roi = Roi()
+        downscaled_roi.params = (new_x, new_y, new_w, new_h)
+        return downscaled_roi
 
+
+if __name__ == '__main__':
+    class Frame: pass
+    frame = Frame
+    frame.shape = [640, 480, 3]
+    r = Roi.from_frame(frame, SIDE.RIGHT, 100)
+    print(r)
+    r = Roi.from_frame(frame, SIDE.LEFT, 100)
+    print(r)
+    r = Roi.from_frame(frame, SIDE.TOP, 100)
+    print(r)
+    r = Roi.from_frame(frame, SIDE.BOTTOM, 100)
+    print(r)
+    print("-----------")
+    r = Roi.from_frame(frame, SIDE.RIGHT, 90)
+    print(r)
+    r = Roi.from_frame(frame, SIDE.LEFT, 90)
+    print(r)
+    r = Roi.from_frame(frame, SIDE.TOP, 90)
+    print(r)
+    r = Roi.from_frame(frame, SIDE.BOTTOM, 90)
+    print(r)
+    print("-----------")
+    r = Roi.from_frame(frame, SIDE.RIGHT, 10)
+    print(r)
+    r = Roi.from_frame(frame, SIDE.LEFT, 10)
+    print(r)
+    r = Roi.from_frame(frame, SIDE.TOP, 10)
+    print(r)
+    r = Roi.from_frame(frame, SIDE.BOTTOM, 10)
+    print(r)
